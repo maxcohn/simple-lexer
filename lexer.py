0c7a1f9d5ddfb7229b86f336828a0c59b30c89e1
Max Cohn <mxc1805@rit.edu>
2019-06-08 22:35:22 -0400

    initial commit

---
+++ lexer.py
@@ -0,0 +1,12 @@
+# lexer.py
+
+"""Inspired by ply and sly"""
+
+class Lex():
+
+    def __init__(self):
+        self.tokens = {}
+
+    def create_regex():
+        """Builds the master regex to construct"""
+    pass
